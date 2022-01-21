1c44a95e27ff19a6590c2d6626b0a5eeb28ca20a
gabrieljmacedo <gabrieljm58@hotmail.com>
2022-01-21 18:31:44 -0300

    inclusao do arquivo jokenpo

---
+++ Jokenpo.java
@@ -0,0 +1,61 @@
+package jokenpo;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Jokenpo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random random = new Random();
+		
+		int valorUsuario, valorAleatorioPc;
+		
+		System.out.println("----------------------------");
+		System.out.println("J O K E N P O");
+		System.out.println("----------------------------");
+		System.out.println("1 - Pedra");
+		System.out.println("2 - Papel");
+		System.out.println("3 - Tesoura");
+		System.out.println("----------------------------");
+		
+		/* Caso usuário ganha soma, caso perca subtrai
+		 * U1 C2 Computador ganha = 3
+		 * U1 C3 Usuário ganha = 2
+		 * U2 C1 Usuário ganha = 3
+		 * U2 C3 Computador ganha = 1
+		 * U3 C1 Computador ganha -2
+		 * U3 C2 Usuário ganha 5
+		*/
+		
+		System.out.println("Jogador 1, escolha sua opção: ");
+		valorUsuario = sc.nextInt();
+				
+		//Escolha do computador
+		valorAleatorioPc = random.nextInt(3) + 1; //Gerando um número em 1 e 3
+		
+				
+		switch(valorAleatorioPc) {
+		case 1:
+			System.out.println("Você escolheu " + valorUsuario + "Computador escolheu pedra");
+			break;
+		case 2:
+			System.out.println("Você escolheu " + valorUsuario + "Computador escolheu papel");
+			break;
+		case 3:
+			System.out.println("Você escolheu " + valorUsuario + "Computador escolheu tesoura!");
+		}
+		
+		if (valorUsuario == valorAleatorioPc) {
+			System.out.println("Empate!");
+		} else if ((valorUsuario + valorAleatorioPc) == 2 && (valorUsuario + valorAleatorioPc == 3)) {
+			System.out.println("Usuário foi o vencedor!");
+		} else {
+			System.out.println("Computador foi o vencedor!");
+		} 
+		
+		sc.close();
+		
+	}
+
+}
